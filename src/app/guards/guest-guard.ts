44663777df6c35ea9c90e77ead8e44beb1e9968b
mrmdshahriar <shahriar.ict@gmail.com>
2026-09-16 12:03:08 +0600

    slslsl

---
+++ src/app/guards/guest-guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+
+
+// export const guestGuard: CanActivateFn = (route, state) => {
+//   return true;
+// };
+
+
+export const guestGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return router.createUrlTree(['/home']);
+  }
+
+  return true;
+};
